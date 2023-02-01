5b7478aef6b87ffb732e30cff8c44807c20ba806
Ivan Doležal <ivan.dolezal@gmail.com>
2023-02-01 20:47:33 +0100

    template wip

---
+++ script.js
@@ -0,0 +1,15 @@
+function bindRegionEvents() {
+    const region = document.querySelector('#kralovehradecky');
+    region.addEventListener('click', (e) => {
+        region.classList.toggle("activeRegion");
+        console.log("click")
+    })
+}
+
+
+
+// -- everything starts here
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Ready");
+    bindRegionEvents()
+});
