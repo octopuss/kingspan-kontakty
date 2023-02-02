793dce0966ad8ab321b0a2725ae2c230d33e439a
Ivan Doležal <ivan.dolezal@gmail.com>
2023-02-02 17:26:36 +0100

    lazyload images

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+function preloadRegionImage(region) {
+    const link = document.querySelector(`preload-${region.id}`)
+       if(!link) {
+        const l = document.createElement("link");
+        l.rel = "preload";
+        l.href =  `./${region.id}.svg`;
+        l.as = "image";
+        l.id =  `preload-${region.id}`;
+        document.head.appendChild(l)
+       } 
+}
+
 function bindRegionEvents() {
     const regions = document.querySelectorAll(".noFill");
    regions.forEach(region => {
     region.addEventListener('click', (e) => {
-       console.log(region.id)
+        preloadRegionImage(region);
        window.location.href = `./${region.id}.html`
    });
+   region.addEventListener('mouseover', (e) => {
+        preloadRegionImage(region);
+   })
 })
 }
 // -- everything starts here
